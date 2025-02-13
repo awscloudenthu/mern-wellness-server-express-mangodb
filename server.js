19772994b57f3a01769d5b699ae0da1cd471e38e
awscloudenthu <awscloudenthu@gmail.com>
2025-02-12 20:33:34 -0800

    Removed seeded data

--- server.js
+++ server.js
@@ -31,53 +31,15 @@ const healthDataSchema = new mongoose.Schema({
 
 const HealthData = mongoose.model('HealthData', healthDataSchema);
 
-// Static health data for seeding
-const healthDataSeed = [
-  { userId: 'seeded', date: "2025-01-20", steps: 5000, calories: 200 },
-  { userId: 'seeded', date: "2025-01-19", steps: 7000, calories: 300 },
-  { userId: 'seeded', date: "2025-01-18", steps: 8000, calories: 350 },
-  { userId: 'seeded', date: "2025-01-17", steps: 10000, calories: 400 },
-  { userId: 'seeded', date: "2025-01-16", steps: 4500, calories: 180 },
-  { userId: 'seeded', date: "2025-01-15", steps: 9000, calories: 360 },
-  { userId: 'seeded', date: "2025-01-14", steps: 12000, calories: 500 }
-];
-
-
-// Seed data if no documents exist
-mongoose.connection.once('open', async () => {
-  const count = await HealthData.countDocuments({ userId: 'seeded' });
-  if (count === 0) {
-    await HealthData.insertMany(healthDataSeed);
-    console.log('Seed data inserted for userId: seeded');
-  }
-});
-
 // API Endpoints
 
 // 1. Get health data for a specific user
-//    For now, only the this get method is used for "seeded" user
-//       Eventually, when aws cognito userpool is supported, rest of the
-//       methods will be used to persist user data
 //
 app.get('/api/health-data/:userId', async (req, res) => {
-  const userId = req.params.userId || 'seeded';
+  const userId = req.params.userId ;
   try {
     const data = await HealthData.find({ userId });
-    const today = new Date(); //   today's date
-    const adjustedData = data.map((entry, index) => {
-      const entryDate = new Date(today); // Copy today's date
-      entryDate.setDate(today.getDate() - index - 2 ); // Subtract days based on the index, just show two days ago
-
-      // Format the date to 'yyyy-mm-dd'
-      const formattedDate = entryDate.toISOString().split('T')[0];
-
-      const updatedEntry = entry.toObject(); // Convert the Mongoose document to plain JavaScript object
-      updatedEntry.date = formattedDate; // Update the date field
-
-      return updatedEntry; // Return the updated entry
-    });
-
-    res.json(adjustedData);
+    res.json(data);
   } catch (error) {
     console.error("Database Error:", error.message);
     res.status(500).json({ error: "Error fetching health data" });
@@ -86,7 +48,9 @@ app.get('/api/health-data/:userId', async (req, res) => {
 
 // 2. Add new health data for a user
 app.post('/api/health-data', async (req, res) => {
+  
   const { userId, date, steps, calories } = req.body;
+
   try {
     const newEntry = new HealthData({ userId, date, steps, calories });
     await newEntry.save();
